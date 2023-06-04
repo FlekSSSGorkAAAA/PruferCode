a15a245879c327388a1fb6c8b48b11c2982acb82
Flek$$$GorkAAAA <@romanman72@gmail.com>
2023-06-04 20:44:07 +0300

    Добавлен неправильно работающий функционал кодирования

--- PruferCode/Program.cs
+++ PruferCode/Program.cs
@@ -21,16 +21,32 @@ namespace PruferCode
 
             using (StreamReader sr = new StreamReader(inputFile))
             {
-                string line = sr.ReadLine();
-                string[] strings = line.Split(',');
-                foreach (string str in strings)
+                string line;
+                while ((line = sr.ReadLine()) != null)
                 {
-                    result.Add(int.Parse(str.Trim()));
+                    result.Add(int.Parse(line.Trim()));
                 }
             }
 
             return result;
         }
+
+        public List<string> ReadDecodeFromFile()
+        {
+            List<string> result = new List<string>();
+
+            using (StreamReader sr = new StreamReader(outputFile))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    result.Add(line.Trim());
+                }
+            }
+
+            return result;
+        }
+
 
         public List<string> DecodePrufer(List<int> code)
         {
@@ -78,11 +94,10 @@ namespace PruferCode
             {
                 lastNumber++;
             }
-            decode.Add($"{lastNumber} -> {n}");
+            decode.Add($"{n} -> {lastNumber}");
 
             return decode;
         }
-
 
         private int FindSmallest(List<int> numbers)
         {
@@ -97,6 +112,59 @@ namespace PruferCode
             return smallest;
         }
 
+        public List<int> EncodePrufer(List<string> decode)
+        {
+            List<int> code = new List<int>();
+            int n = decode.Count + 2;
+            int[] degrees = new int[n + 1];
+
+            // Подсчет степеней вершин
+            foreach (string pair in decode)
+            {
+                string[] vertices = pair.Split(new[] { " -> " }, StringSplitOptions.None);
+                int vertex1, vertex2;
+
+                if (int.TryParse(vertices[0], out vertex1) && int.TryParse(vertices[1], out vertex2))
+                {
+                    degrees[vertex1]++;
+                    degrees[vertex2]++;
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка: неверный формат вершин в файле decode.txt.");
+                    return code;
+                }
+            }
+
+            // Кодирование Прюфера
+            for (int i = 0; i < n - 2; i++)
+            {
+                int smallest = Array.FindIndex(degrees, d => d == 1);
+
+                if (smallest == -1)
+                {
+                    smallest = Array.FindIndex(degrees, d => d != 0); // Выбрать первый доступный индекс, не равный 0
+                }
+
+                string[] vertices = decode[i].Split(new[] { " -> " }, StringSplitOptions.None);
+                int vertex1, vertex2;
+
+                if (int.TryParse(vertices[0], out vertex1) && int.TryParse(vertices[1], out vertex2))
+                {
+                    code.Add(vertex2);
+                    degrees[smallest]--;
+                    degrees[vertex1]--;
+                    degrees[vertex2]--;
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка: неверный формат вершин в файле decode.txt.");
+                    return code;
+                }
+            }
+
+            return code;
+        }
 
         public void WriteDecodeToFile(List<string> decode)
         {
@@ -110,6 +178,19 @@ namespace PruferCode
 
             Console.WriteLine("Код Прюфера успешно декодирован и записан в файл.");
         }
+
+        public void WriteCodeToFile(List<int> code)
+        {
+            using (StreamWriter sw = new StreamWriter(inputFile))
+            {
+                foreach (int num in code)
+                {
+                    sw.WriteLine(num);
+                }
+            }
+
+            Console.WriteLine("Закодированный код Прюфера успешно записан в файл.");
+        }
     }
 
     class Program
@@ -121,8 +202,8 @@ namespace PruferCode
             while (true)
             {
                 Console.WriteLine("Что вы желаете?");
-                Console.WriteLine("1 - закодировать (нужно заполнить файл code.txt)");
-                Console.WriteLine("2 - декодировать (нужно заполнить файл decode.txt)");
+                Console.WriteLine("1 - закодировать (нужно заполнить файл decode.txt)");
+                Console.WriteLine("2 - декодировать (нужно заполнить файл code.txt)");
                 Console.WriteLine("3 - выход из программы");
                 int choice = Convert.ToInt32(Console.ReadLine());
 
@@ -130,6 +211,9 @@ namespace PruferCode
                 {
                     case 1:
                         Console.WriteLine("Кодирование:");
+                        List<string> decodeToEncode = prufer.ReadDecodeFromFile();
+                        List<int> encoded = prufer.EncodePrufer(decodeToEncode);
+                        prufer.WriteCodeToFile(encoded);
                         break;
                     case 2:
                         Console.WriteLine("Декодирование:");
